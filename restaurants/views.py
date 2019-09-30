cd63aed7c6877d67aec013a5e4ec72738fd2d4eb
Nour El-Sharkawi <Nour@Nours-MacBook-Pro-703.local>
2019-09-30 19:19:05 +0300

    task done

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def say_hello(request):
+	return HttpResponse("<h1>Hello, world!</h1>")
+
+def render_function(request):
+	context = {
+	"msg": "Hello World!"
+	}
+	return render(request, "html_file.html", context)
